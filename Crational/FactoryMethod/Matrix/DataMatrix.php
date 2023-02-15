aa191a5a6faf136b26262906f6c9d1880b09baa8
kosi2109slmm <sithuhtet@securelinkmm.com>
2023-02-15 08:13:32 +0630

    add factory method

---
+++ Crational/FactoryMethod/Matrix/DataMatrix.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Creational\FactoryMethod\Matrix;
+
+use Creational\FactoryMethod;
+
+class DataMatrix extends FactoryMethod\InformationMatrix
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
